9d1bb67e9929a833301913a4cbd029feda396107
Baz O'Mahony <bomahony@shutterstock.com>
2014-09-24 01:02:24 -0600

    More templating

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,7 +2,6 @@ var excuse_model = require('../lib/excuse');
 
 function getIndex(req,res) {
     excuse_model.getRandom(function(err,excuse) {
-        console.log(excuse.excuse);
         res.render("index.html",excuse);
     });
 }
